62da0bff5156c73c08ec66a1bb009ed487aaec7e
Ginger King <briansherbst@gmail.com>
2024-07-04 00:12:50 -0600

    revert vue config change

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/bestbale.github.io/',
   css: {
     loaderOptions: {
       sass: {
