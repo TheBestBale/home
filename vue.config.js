7da5207a3cd01d98fd8cd4761f0f3e7e58317424
Ginger King <briansherbst@gmail.com>
2024-07-03 23:21:04 -0600

    changed public path

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/home',
+  publicPath: '/bestbale.github.io/',
   css: {
     loaderOptions: {
       sass: {
