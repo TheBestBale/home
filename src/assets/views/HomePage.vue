56597896f4029d04c526b25a3c275cd55870329d
Barbie <briansherbst@gmail.com>
2025-10-27 22:22:07 -0600

    feat: added a quick home screen

--- src/assets/views/HomePage.vue
+++ src/assets/views/HomePage.vue
@@ -1,14 +1,48 @@
 <template>
-  <div>Home</div>
+  <div class="home">
+    <div class="home-text">
+      <div>
+        Welcome To BestBale
+      </div>
+      <div>
+        With over 60 years combined farming and ranching experience, we hope to bring you the best service available.
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+
 export default {
   name: "HomePage",
+  components: {},
 }
 </script>
 
 
 <style scoped lang="scss">
+.home {
+  display: flex;
+  flex-direction: column;
+  background: url("@/assets/images/backsplash.jpg");
+  background-size: cover; /* Scales the image to cover the entire div */
+  background-position: center; /* Centers the image */
+  background-repeat: no-repeat; /* Prevents image repetition */
+  width: 100%;
+  height: 100%;
+
+  &-text {
+    width: 75%;
+    border-radius: 12px;
+    padding: 24px;
+    color: $color-black;
+    font-size: 3rem;
+    background-color: $color-secondary-opacity;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    margin: auto  ;
+  }
+}
 
 </style>
